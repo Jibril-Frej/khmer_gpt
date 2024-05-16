ec4e3bb1a6f452c52951232490a330d777c8be56
Jibril-Frej <frejjibril@gmail.com>
2024-05-16 22:08:13 +0200

    finish notebooks

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def complete_text(input_text, model, tokenizer):
+def complete_text(input_text, model, tokenizer, num_return_sequences=1):
     input_ids = torch.tensor([tokenizer.encode(input_text).ids])
     attention_mask = torch.ones_like(input_ids)
     model.eval()
@@ -10,11 +10,11 @@ def complete_text(input_text, model, tokenizer):
         attention_mask=attention_mask,
         max_length=64,
         num_beams=10,
-        num_return_sequences=10,
+        num_return_sequences=num_return_sequences,
         output_scores=True,
         return_dict_in_generate=True,
     )
 
-    for score, prediction in zip(output.sequences_scores, output.sequences):
+    for _, prediction in zip(output.sequences_scores, output.sequences):
         predicted_text = tokenizer.decode(prediction.tolist(), skip_special_tokens=True)
-        print(score.numpy(), predicted_text)
+        print(predicted_text)
